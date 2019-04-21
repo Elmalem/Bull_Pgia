22962ecdf226293bc467f8d8be27f17c5bab57a8
BarGenish <44568486+bargenish44@users.noreply.github.com>
2019-04-21 21:47:08 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -1,20 +1,29 @@
 #include <string>
-#include <iostream>
 #include"calculate.hpp"
-
 using namespace std;
+using namespace bullpgia;
 
-string bullpgia::calculateBullAndPgia(const string& choice,const string& guess){
-  int bull = 0;
-  int pgia = 0;
-  for (size_t i = 0; i < choice.length(); i++) {
-    for (size_t j = 0; j < guess.length(); j++) {
-      if( i == j && choice[i] == guess[j])
-      bull++;
-      else if( i != j && choice[i] == guess[j])
-      pgia++;
-    }
+string bullpgia::calculateBullAndPgia(const string choice,const string guess){
+  int bull = 0,pgia=0,i;
+  int arrchoice[10],arrguess[10];
+  for (i=0;i<10;i++){
+    arrchoice[i]=0,arrguess[i]=0;
   }
-  string ans = to_string(bull) + "," + to_string(pgia);
-  return ans;
+  for(i=0;i<choice.length();i++){
+    if(choice[i]==guess[i])
+      bull++;
+  }
+  for(i=0;i<choice.length();i++){
+    int temp=(choice[i])-'0';
+    arrchoice[temp]++;
+  }
+  for (i=0;i<guess.length();i++){
+    int temp=(guess[i])-'0';
+    arrguess[temp]++;
+  }
+  for (i=0;i<10;i++){
+    pgia+=min(arrchoice[i],arrguess[i]);
+  }
+  pgia-=bull;
+  return to_string(bull) + "," + to_string(pgia);
 }
