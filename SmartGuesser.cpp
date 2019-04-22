a011051d5245f9fe5123e59e8572628ab798beae
Elmalem <44621326+Elmalem@users.noreply.github.com>
2019-04-22 14:45:25 +0300

    Add files via upload

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -3,33 +3,40 @@
 
 using namespace std;
 
-void bullpgia::SmartGuesser:: learn(string solutions) {
-  size_t i = solutions.find(',');
-  int x = stoi(solutions.substr(0,i));
-  int y = stoi(solutions.substr(i+1,solutions.length()));
-  bull += x;
-  pgia += y;
-  game_on = false;
+// exapmle for it - "4,0" present 4 bull and 0 pgia
+void bullpgia::SmartGuesser:: learn(string solutions) { // learn() get the "bull,pgia" format string
+  size_t i = solutions.find(','); // the seperator
+  int x = stoi(solutions.substr(0,i)); // bulls
+  int y = stoi(solutions.substr(i+1,solutions.length())); // pgias
+  bull += x; // add to the private member(bull)
+  pgia += y; // add to the private m  ember(pgia)
+  game_on = false; // we finished the round
 }
 
 void bullpgia::SmartGuesser:: startNewGame(uint length) {
-  game_on = true;
+  game_on = true; // game on
 }
 
-string bullpgia::SmartGuesser:: guess(){
-  if(!game_on){
-    if(bull == 1){
+string bullpgia::SmartGuesser:: guess(){ // it's a smart guess ,according to bull and pgia we will know what we need to guess
+  if(!game_on){ // if it's time to guess now
+    switch (bull) {
+      case 1:{
 
-    }
-    else if(bull == 2){
+        break;
+      }
+      case 2:{
 
-    }
-    else if(bull == 3){
+        break;
+      }
+      case 3:{
 
-    }
-    else if(bull == 4){
-      
+        break;
+      }
+      case 4:{
+
+        break;
+      }
     }
     return "1234";}
-  else{return "sorry,wait for the end of the round";}
+  else{return "sorry,wait for the end of the round";} // it's no a game time
 }
