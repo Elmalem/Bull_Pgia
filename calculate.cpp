f443b3703e92ada8645b82d526e16b239c097774
BarGenish <44568486+bargenish44@users.noreply.github.com>
2019-04-30 12:32:02 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -5,20 +5,28 @@ using namespace bullpgia;
 
 string bullpgia::calculateBullAndPgia(const string choice,const string guess){
   int bull = 0,pgia=0,i;
+  string ans="";
   int arrchoice[10],arrguess[10];
+  if(choice.size()<guess.size()){
+  	for(int i=0;i<choice.size();i++){
+		ans+=guess.at(i);
+	}
+  }
+  else
+	ans=guess;
   for (i=0;i<10;i++){
     arrchoice[i]=0,arrguess[i]=0;
   }
   for(i=0;i<choice.length();i++){
-    if(choice[i]==guess[i])
+    if(choice[i]==ans[i])
       bull++;
   }
   for(i=0;i<choice.length();i++){
     int temp=(choice[i])-'0';
     arrchoice[temp]++;
   }
-  for (i=0;i<guess.length();i++){
-    int temp=(guess[i])-'0';
+  for (i=0;i<ans.length();i++){
+    int temp=(ans[i])-'0';
     arrguess[temp]++;
   }
   for (i=0;i<10;i++){
